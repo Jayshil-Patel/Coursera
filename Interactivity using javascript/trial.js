3a115e2e6b12f4c556581032327bdee507a56aa2
Jayshil Patel <jayshiljatin@gmail.com>
2021-09-25 22:59:17 +0530

    updated few files

--- Interactivity using javascript/trial.js
+++ Interactivity using javascript/trial.js
@@ -1,4 +1,3 @@
-document.write("Are you krenil?")
-var wid = document.getElementById('para');
-var sdffds = document.getElementBY
-console.log(22==22);
+function logger(element){
+    console.log(element.id);
+}
