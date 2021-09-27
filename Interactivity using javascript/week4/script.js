eb8365799d71cba9571256a500682d17561752de
Jayshil Patel <jayshiljatin@gmail.com>
2021-09-27 14:08:58 +0530

    added interactivity

---
+++ Interactivity using javascript/week4/script.js
@@ -0,0 +1,11 @@
+function billingFunction() {
+  if (document.getElementById("same").checked) {
+    document.getElementById("billingName").value =
+      document.getElementById("shippingName").value;
+    document.getElementById("billingZip").value =
+      document.getElementById("shippingZip").value;
+  } else {
+    document.getElementById("billingName").value = null;
+    document.getElementById("billingZip").value = null;
+  }
+}
