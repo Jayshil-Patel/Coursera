c1d0bb3c44e58e101e74c855c0e02b0fa2de958b
Jayshil Patel <jayshiljatin@gmail.com>
2021-09-27 13:44:48 +0530

    updated a folder

---
+++ Interactivity using javascript/week3/script.js
@@ -0,0 +1,7 @@
+function check(){
+    var em1 = document.getElementById("email_addr");
+    var em2 = document.getElementById("email_repeat");
+    if(em1.value != em2.value){
+        alert("Emails does not match");
+    }
+}
